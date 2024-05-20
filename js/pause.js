a1ab3d9ca49e4cd60a1e617c4c62fbf479c63178
firstpotato2 <firstpotato2.0@example.com>
2024-05-20 12:39:57 +0800

    Imitial commit with HTML and CSS

--- js/pause.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('resumeButton').addEventListener('click', function() {
-    const currentImageIndex = sessionStorage.getItem('currentImageIndex');
-    window.location.href = `test.html?index=${currentImageIndex}`;
-});
